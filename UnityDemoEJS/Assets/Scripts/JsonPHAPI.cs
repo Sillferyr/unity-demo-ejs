4d961e882887d4b9bd064e84c511fbdb907180e2
Sillferyr <sillferyr@gmail.com>
2018-03-27 18:38:54 -0300

    Session 6 End: Scene 3 ready. Proposed multiple last posts as extra for later.

--- UnityDemoEJS/Assets/Scripts/JsonPHAPI.cs
+++ UnityDemoEJS/Assets/Scripts/JsonPHAPI.cs
@@ -8,6 +8,7 @@ public class JsonPHAPI : MonoBehaviour {
 
     public Text input;
     public Text output;
+    public GameObject outputRawImage;
     public int apiNumber = 3; // 3 for JsonPlaceHolder, 4 for self-made
 
     // Use this for initialization
@@ -32,7 +33,7 @@ public class JsonPHAPI : MonoBehaviour {
         string inStr = input.text;
         int.TryParse(inStr, out id);
         if(id == 0)
-            Debug.LogError("Post number must be integer ");
+            Debug.LogError("Post number must be an integer  ");
 
         string url;
 
@@ -73,45 +74,56 @@ public class JsonPHAPI : MonoBehaviour {
             } else {
                 //added "results : " for parsing with SimpleJSON
                 var N = JSON.Parse(" \"results\" : " + result);
-                Debug.Log("Api" + apiNumber + "return Parsed");
-                Debug.Log(N.ToString());
+                Debug.Log("Api" + apiNumber + " Parsed");
+                //Debug.Log(N.ToString());
 
+                string dataOut = "";
                 switch (apiNumber)
                 {
                     case 3:
-                        string commentData = "";
                         //N[i][j] where i stands for different comments and j stands for data in the comment i (J = 0 : "postid", 1:"id", 2:"name", 3:"email", 4 :"body")
                         for (int i = 0; !string.IsNullOrEmpty(N[i][0]); i++)
                         {
-                            commentData = "\nId: " + N[i][1] + "\nName: " + N[i][2] + "\nEmail: " + N[i][3] + " \nBody: " + N[i][4] + "\n";
-                            updateOutput(commentData);
+                            dataOut = "\nId: " + N[i][1] + "\nName: " + N[i][2] + "\nEmail: " + N[i][3] + " \nBody: " + N[i][4] + "\n";
+                            updateOutput(dataOut);
                         }
                         
                         break;
 
-                    case 4:
-                        //TODO:  parse lasts posts by inputParams(how many last posts), get their data and call their thumb images by postId
+                    //parse lasts posts by inputParams(how many last posts), get their data and call their thumb images by postId
+                    case 4:                    
                         int length = N.Count;
                         int posts_req = inputParams;
                         int posts_rdy = 0;
                         //N[i][j] where i stands for different posts and j stands for data in the post i (J= 0 : "userid", 1 : "id", 2 : "title", 3 : "body")
-                        for (int i = length -1 ;  posts_rdy++ >= posts_req ; i--)
+                        for (int i = length -1 ;  posts_rdy++ < posts_req ; i--)
                         {
                             //debug
-                            commentData = "\nUserid: " + N[i][0] +"\nId: " + N[i][1] + "\nTitle: " + N[i][2] + "\nBody: " + N[i][3] + "\n";
-                            Debug.Log(commentData);
-                            updateOutput(commentData);
-                            //TODO: updateOutputPhoto();
+                            dataOut = "\nUserid: " + N[i][0] +"\nId: " + N[i][1] + "\nTitle: " + N[i][2] + "\nBody: " + N[i][3] + "\n";
+                            updateOutput(dataOut);
+
+                            //call for photos
+                            StartCoroutine( callPHApi("https://my-json-server.typicode.com/Sillferyr/unity-demo-api/photos?postId=" + N[i][1], 41, 0) );
+
                         }
 
-
-                        //call for photos
-                        //callPHApi("https://my-json-server.typicode.com/Sillferyr/unity-demo-api/photos?postId="+postId , 41)
                         break;
 
                     case 41:
-                        //TODO: another api call for photos...
                         Debug.Log("Call Api4: last posts-> get thumb images");
+                        //N[0][j] where 0 stands for the first photo and j stands for data in the photo i (J = 0 : "postid", 1:"id", 2:"title", 3:"url", 4 :"thumbnailurl")                     
+                        string thumbnailUrl = N[0][4];
+                        //TODO: load picture from url
+                        Texture2D tex;
+                        tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+                        using ( WWW thumbWWW = new WWW(thumbnailUrl) )
+                        {
+                            yield return thumbWWW;
+                            thumbWWW.LoadImageIntoTexture(tex);
+                            updateOutputPhoto(tex);
+                            //GetComponent<Renderer>().material.mainTexture = tex;
+
+                        }
 
                         break;
 
@@ -123,13 +135,15 @@ public class JsonPHAPI : MonoBehaviour {
         }
     }
 
-
+    //extra: instantiate new gameobject instead
     public void updateOutput(string str)
     {
         output.text += str;
     }
 
+    //extra: instantiate new gameobject instead
     public void updateOutputPhoto(Texture2D image) {
-        //TODO
+        Debug.Log("updateOutputPhoto call ");
+        outputRawImage.GetComponent<RawImage>().texture = image;
     }
 }
